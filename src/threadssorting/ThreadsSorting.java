880842ec9ddac607f72e6b1f407313df9ebbfb5c
Yusuf YURTSEVER <yusuf.mh@outlook.com>
2017-05-08 15:15:34 +0300

    2nd  Commit

--- src/threadssorting/ThreadsSorting.java
+++ src/threadssorting/ThreadsSorting.java
@@ -1,19 +1,18 @@
 package threadssorting;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-
-
-public class ThreadsSorting {
-    /*
-    public  static void divideListByTwo(List list){
+class Twolists{
+        static List sub1 = new LinkedList();
+        static List sub2 = new LinkedList();
+}
+ class MyThread {
+   
+    public void divideListByTwo(List list){
         int sizeOfList = list.size();
         Twolists.sub1 = new LinkedList();
         Twolists.sub2 = new LinkedList();
@@ -21,32 +20,14 @@ public class ThreadsSorting {
             Twolists.sub1.add(list.get(i));
         }
         for (int i = (sizeOfList/2); i <= sizeOfList-1; i++) {
-                        Twolists.sub2.add(list.get(i));
-        }
-    }
-    
-    public static List sortList(List list){
-                Collections.reverse(list);
-                Collections.sort(list);
-                return list;
-    }
-    
-    public static List mergeLists(){
-        List merged =  (List) Stream.concat(Twolists.sub1.stream(), Twolists.sub2.stream()).collect(Collectors.toList());
-        return sortList(merged);
-    }
-    public static List ReversedOrder(List list){
-        Collections.sort(list, Collections.reverseOrder());
-        return list;
-    }
- */
-    
-    public static void main(String[] args) {
-  
-        /*
-             LinkedList TheList = new LinkedList(Arrays.asList(7,12,19,3,18,4,2,6,15,8));
-
-            divideListByTwo(TheList);
+            Twolists.sub2.add(list.get(i));
+        }      
+        System.out.println("the list is Divided By Two List...");
+       
+            System.out.println("\nOriginal List");
+              list.forEach((Item) -> {
+            System.out.print("["+ Item.toString() + "]");
+        }); 
             System.out.println("\nSub1 List");
               Twolists.sub1.forEach((Item) -> {
             System.out.print("["+ Item.toString() + "]");
@@ -54,24 +35,79 @@ public class ThreadsSorting {
                  System.out.println("\nSub2 List");
               Twolists.sub2.forEach((Item) -> {
             System.out.print("["+ Item.toString() + "]");
+        });               
+    }
+      public MyThread(List list){
+          divideListByTwo(list);
+     }
+}
+class SubList1Thread implements Runnable{
+ public  List sortList(List list){
+                Collections.reverse(list);
+                Collections.sort(list);
+                return list;
+    }
+    @Override
+    public void run() {
+        sortList(Twolists.sub1);
+        System.out.println("\nSorted Sub1 List");
+        sortList(Twolists.sub1).forEach((Item) -> {
+        System.out.print("["+ Item.toString() + "]");
         }); 
-              System.out.println("\nSorted Sub1 List");
-                  sortList(Twolists.sub1).forEach((Item) -> {
-            System.out.print("["+ Item.toString() + "]");
+    }
+    
+}
+class SubList2Thread implements Runnable{
+ public  List sortList(List list){
+                Collections.reverse(list);
+                Collections.sort(list);
+                return list;
+    }
+    @Override
+    public void run() {
+        sortList(Twolists.sub2);
+        System.out.println("\nSorted Sub2 List");
+        sortList(Twolists.sub2).forEach((Item) -> {
+        System.out.print("["+ Item.toString() + "]");
         }); 
-                    System.out.println("\nSorted Sub2 List");
-                  sortList(Twolists.sub2).forEach((Item) -> {
-            System.out.print("["+ Item.toString() + "]");
-        }); 
-                    System.out.println("\nMerged & Sorted (sub1,sub2) List");
+    }
+    
+}
+class MergeSubLists implements Runnable{
+     public  List sortList(List list){
+                Collections.reverse(list);
+                Collections.sort(list);
+                return list;
+    }
+  public  List mergeLists(){
+        List merged =  (List) Stream.concat(Twolists.sub1.stream(), Twolists.sub2.stream()).collect(Collectors.toList());
+        return sortList(merged);
+    }
+    public  List ReversedOrder(List list){
+        Collections.sort(list, Collections.reverseOrder());
+        return list;
+    }
+    @Override
+    public void run() {
+            mergeLists();
+                System.out.println("\nMerged & Sorted (sub1,sub2) List");
                   mergeLists().forEach((Item) -> {
             System.out.print("["+ Item.toString() + "]");
         }); 
-                   System.out.println("\n Ascending order :Merged & Sorted (sub1,sub2) List");
+                System.out.println("\n Ascending order :Merged & Sorted (sub1,sub2) List");
                   ReversedOrder(mergeLists()).forEach((Item) -> {
             System.out.print("["+ Item.toString() + "]");
         }); 
-    */
+    }
+    
+}
+
+
+
+public class ThreadsSorting {
+   
+    public static void main(String[] args) {
+  
         LinkedList TheList = new LinkedList(Arrays.asList(7,12,19,3,18,4,2,6,15,8));
         MyThread t1 = new MyThread(TheList);
         SubList1Thread sublist1 = new SubList1Thread();
